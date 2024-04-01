7f1933f3e628fed1286da9e7a534b2114d20fa7b
fabiologe <fabioqu@tutanota.com>
2024-04-01 15:44:39 +0200

    renamed:    back/requirments.txt -> back/requirements.txt 	modified:   front/vue/package-lock.json 	modified:   front/vue/package.json 	modified:   front/vue/src/App.vue 	deleted:    front/vue/src/components/HelloWorld.vue 	deleted:    front/vue/src/components/UtmMap.vue 	deleted:    front/vue/src/components/test_1.tif

--- front/vue/src/App.vue
+++ front/vue/src/App.vue
@@ -2,7 +2,7 @@
 import TiffUpload from './components/TiffUpload.vue'
 import JpgUpload from './components/JpgUpload.vue'
 import GpsMap from './components/GpsMap.vue'
-import UtmMap from './components/UtmMap.vue'
+
 </script>
 
 <template>
@@ -17,37 +17,11 @@ import UtmMap from './components/UtmMap.vue'
       <TiffUpload />
       <JpgUpload />
       <GpsMap/>
-      <!--<UtmMap/>-->
     </main>
   </div>
 </template>
 <style scoped>
-header {
-  line-height: 1;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
 
 
